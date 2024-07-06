9badfc3623c9463d429f2122b6dd8eded1c951fb
Tankica <tankicaponova@gmail.com>
2024-07-06 19:37:14 +0200

    added component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { MainComponent } from "./main/main.component";
 import {  Products_bd } from './products';
 import { ProductComponent } from "./product/product.component";
 import { Product } from './product/product.model';
+import { ContainerComponent } from "./container/container.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, MainComponent, ProductComponent]
+    imports: [RouterOutlet, NavbarComponent, MainComponent, ProductComponent, ContainerComponent]
 })
 export class AppComponent {
   title = 'nov';
