d73b7b18f6f58971e2667c2b425c2e2624d92920
Tankica <tankicaponova@gmail.com>
2024-07-09 20:44:51 +0200

    footer

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { Products_bd } from './products';
+import { ProductComponent } from "./product/product.component";
 
 @Component({
-  selector: 'app-container',
-  standalone: true,
-  imports: [],
-  templateUrl: './container.component.html',
-  styleUrl: './container.component.css'
+    selector: 'app-container',
+    standalone: true,
+    templateUrl: './container.component.html',
+    styleUrl: './container.component.css',
+    imports: [ProductComponent]
 })
 export class ContainerComponent {
-  
+  products=Products_bd;
+
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
