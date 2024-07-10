5e55c7e0f3d903223184c44a99dbb464fca9b3fd
Tankica <tankicaponova@gmail.com>
2024-07-10 04:31:07 +0200

    details

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -3,15 +3,36 @@ import { Products_bd } from './products';
 import { ProductComponent } from "./product/product.component";
 import { FormsModule } from '@angular/forms';
 import { FilterpipePipe } from '../filterpipe.pipe';
+import { Product } from './product/product.model';
 @Component({
     selector: 'app-container',
     standalone: true,
     templateUrl: './container.component.html',
     styleUrl: './container.component.css',
-    imports: [ProductComponent,FormsModule,FilterpipePipe]
+    imports: [ProductComponent, FormsModule, FilterpipePipe]
 })
 export class ContainerComponent {
   products=Products_bd;
   filterText = '';
 
+  selectedProduct?: Product
+  openModal(product: any): void {
+    this.selectedProduct = product;
+    const modal = document.getElementById('productModal');
+    if(modal){
+    modal.style.display = 'block';
+    }
+  }
+
+  closeModal(): void {
+    const modal = document.getElementById('productModal');
+    if(modal){
+    modal.style.display = 'none';
+    }
+  }
+
+    get ImagePath(){
+      return 'pictures/'+this.selectedProduct?.picture;
+    }
+    
 }
